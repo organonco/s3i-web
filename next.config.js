812894ddad29c5c9b836119efd002851dc494b8b
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-02-08 09:49:47 +0300

    fix domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: { domains: ['thisdomainisonlyfortestingpurposes.xyz'], formats: ['image/avif', 'image/webp'], },
+    images: { domains: ['www.s3i.training'], formats: ['image/avif', 'image/webp'], },
 }
 
 module.exports = nextConfig
