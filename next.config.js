cca3f8ab9fc5e9581fd3dc8c4e537c3e9632d363
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-02-14 01:57:30 +0300

    .

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: { ignoreBuildErrors: true },
-    output: 'export',
     images: { domains: ['www.s3i.training'], formats: ['image/avif', 'image/webp'], unoptimized: true },
 }
 
