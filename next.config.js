3adbdd1fedd8fda45eaa971635f8c0f6330017e7
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-02-15 02:54:34 +0300

    fix images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: { ignoreBuildErrors: true },
-    images: { domains: ['www.s3i.training'], formats: ['image/avif', 'image/webp'], unoptimized: true },
+    images: { domains: ['admin.s3i.training'], formats: ['image/avif', 'image/webp'], unoptimized: true },
 }
 
 module.exports = nextConfig
