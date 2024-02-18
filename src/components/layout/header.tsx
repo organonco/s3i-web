fbe6f0e1ae6d8491455471f2ee2b71056932a85d
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-02-18 04:03:54 +0300

    fix header bug

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -26,7 +26,11 @@ export function Header({ lng }: Props) {
     const { authenticatedStatus, notificationNumber, profileInfo, logUserOut } = useCoursesStore()
     let pathname = usePathname().split('/')[2] ?? ''
     pathname = '/' + pathname
-    const tabArray = [sitemap.home.url, sitemap.my_courses.url, sitemap.courses.index.url, sitemap.teachers.url, sitemap.partners.url, sitemap.about_us.url]
+
+    const tabArray =
+        authenticatedStatus == USER_STATUS.LOGGED_IN ?
+            [sitemap.home.url, sitemap.my_courses.url, sitemap.courses.index.url, sitemap.teachers.url, sitemap.partners.url, sitemap.about_us.url] :
+            [sitemap.home.url, sitemap.courses.index.url, sitemap.teachers.url, sitemap.partners.url, sitemap.about_us.url]
 
     const { t } = useTranslation(lng, 'translation')
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
@@ -52,12 +56,12 @@ export function Header({ lng }: Props) {
     const siteIcon = require('/src/assets/logo.png');
 
     return (
-        <Box sx={{ width: '100%', display: 'fixed', justifyContent: 'space-between', flexDirection: 'row', alignSelf: 'center', paddingTop: 8, paddingX: 10, marginBottom: 0, backgroundColor: 'white'}}>
+        <Box sx={{ width: '100%', display: 'fixed', justifyContent: 'space-between', flexDirection: 'row', alignSelf: 'center', paddingTop: 8, paddingX: 10, marginBottom: 0, backgroundColor: 'white' }}>
             <Box>
                 <Tabs value={tabArray.indexOf(pathname) ?? 0} onChange={handleChange} aria-label="nav tabs example" TabIndicatorProps={{ style: { width: '5%', marginRight: 40, marginLeft: 50, height: 4, borderRadius: 20 } }}>
-                    <LinkTab label={t('pages.' + sitemap.home.title)} href={sitemap.home.url}/>
-                    {authenticatedStatus == USER_STATUS.LOGGED_IN  && 
-                    <LinkTab label={t('pages.' + sitemap.my_courses.title)} href={sitemap.my_courses.url} />
+                    <LinkTab label={t('pages.' + sitemap.home.title)} href={sitemap.home.url} />
+                    {authenticatedStatus == USER_STATUS.LOGGED_IN &&
+                        <LinkTab label={t('pages.' + sitemap.my_courses.title)} href={sitemap.my_courses.url} />
                     }
                     <LinkTab label={t('pages.' + sitemap.courses.index.title)} href={sitemap.courses.index.url} />
                     <LinkTab label={t('pages.' + sitemap.teachers.title)} href={sitemap.teachers.url} />
@@ -65,7 +69,7 @@ export function Header({ lng }: Props) {
                     <LinkTab label={t('pages.' + sitemap.about_us.title)} href={sitemap.about_us.url} />
                 </Tabs>
             </Box>
-            <Box sx={{ display: 'fixed', justifyContent: 'space-between', flexDirection: 'row', alignSelf: 'center', backgroundColor: 'success.light', marginBottom: 10}} gap={4}>
+            <Box sx={{ display: 'fixed', justifyContent: 'space-between', flexDirection: 'row', alignSelf: 'center', backgroundColor: 'success.light', marginBottom: 10 }} gap={4}>
                 <Box>
                     {authenticatedStatus === USER_STATUS.LOGGED_IN ? <Grid container alignItems={'center'} gap={2}>
                         <UserProfilePopper />
@@ -85,7 +89,7 @@ export function Header({ lng }: Props) {
                             <NotificationList open handleClick={handleClick} />
                         </Popper>
                     </Grid>
-                        : authenticatedStatus === USER_STATUS.NOT_LOGGEN_IN ? <Button variant="outlined" style={{color: "#329996", padding: 10, paddingRight: 40, paddingLeft: 40, fontSize: 24, marginTop: 0}} onClick={() => push(sitemap.login.url)}>{t('buttons.login')}</Button>
+                        : authenticatedStatus === USER_STATUS.NOT_LOGGEN_IN ? <Button variant="outlined" style={{ color: "#329996", padding: 10, paddingRight: 40, paddingLeft: 40, fontSize: 24, marginTop: 0 }} onClick={() => push(sitemap.login.url)}>{t('buttons.login')}</Button>
                             : <></>}
 
                 </Box>
