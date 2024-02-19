bcf10763077a80b52f39112952ed84d662a12ef7
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-02-19 17:53:03 +0300

    add who are we

--- src/app/[lng]/about-us/page.tsx
+++ src/app/[lng]/about-us/page.tsx
@@ -1,12 +1,42 @@
+"use client";
+import { Divider, Grid, Typography } from "@mui/material";
 import Link from "next/link";
+import { useTranslation } from "@/app/i18n/client";
 
 export default function Page({ params: { lng } }) {
+
+
+    const { t } = useTranslation(lng, 'translation')
+
+    const sections = ["intro", "who_are_we", "vision", "message", "values", "outro"]
+
+    const styles =  {
+        "labels": {
+          "textAlign": "right",
+          "paddingTop": 4,
+          "marginBottom": 2,
+          "fontSize": 25,
+            "color": "#2D6276",
+          "textDecorationLine": "underline"
+        },
+        "text": {
+          "textAlign": "right",
+          "paddingBottom": 4,
+          "color": "#666666"
+        }
+    }
+
     return (
-        <>
-            <h1>Hi from second page!</h1>
-            <Link href={`/${lng}`}>
-                back
-            </Link>
-        </>
+        <Grid container px={20}>
+            {
+                sections.map((section) =>
+                    <Grid item key={section}>
+                        <Typography sx={styles.labels}>{t('who-are-we.' + section + ".label")}</Typography>
+                        <Typography sx={styles.text}>{t('who-are-we.' + section + ".text")}</Typography>
+                        <Divider></Divider>
+                    </Grid>
+                )
+            }
+        </Grid>
     )
 }
