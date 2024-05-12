0e2f9e000255154260fe8d87bab59d28375e8d40
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-05-12 22:19:57 +0300

    fix teachers and courses card

--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -3,22 +3,87 @@ import { FC } from "react";
 import { StyledImage } from ".";
 import { StyledBox } from "./box";
 
-interface props { title: string, subTitle: string, imageURL: string, isSubscribed: boolean | null | undefined }
-
+interface props {
+  title: string;
+  subTitle: string;
+  imageURL: string;
+  isSubscribed: boolean | null | undefined;
+}
 
 export const CourseCard: FC<props> = (props) => {
-    return (
-        <StyledBox backgroundColor={'primary.main'}>
-            <div style={{ width: '100%', position: 'relative', objectFit: 'cover', height: "100%" }}>
-                <StyledImage imageURL={props.imageURL} />
-            </div>
-            <Box sx={{ width: '100%', justifyContent: 'space-between', display: 'flex', flexDirection: 'column', gap: 1, paddingX: 2, paddingTop: 1, paddingBottom: 3 }}>
-                <Box sx={{ justifyContent: 'space-between', display: 'flex', flexDirection: 'row', gap: 1, alignItems: 'center' }}>
-                    <Typography color='white' fontWeight={'bold'} fontSize={18} textAlign={'right'}>{props.title}</Typography>
-                    {!props.isSubscribed && <Typography variant="caption" color="white" fontWeight={'bold'} sx={{ backgroundColor: 'primary.main', borderRadius: 5, paddingY: 1, paddingX: 2, width: 120, height: 40, borderColor: 'white', borderWidth: 1 }}> سجل الآن </Typography>}
-                </Box>
-                <Typography color='white' fontWeight={'bold'} fontSize={12} textAlign={'right'} textOverflow={'ellipsis'} noWrap >{props.subTitle}</Typography>
-            </Box>
-        </StyledBox>
-    )
-}
+  return (
+    <StyledBox backgroundColor={"primary.main"}>
+      <div
+        style={{
+          width: "100%",
+          position: "relative",
+          objectFit: "cover",
+          height: "100%",
+        }}
+      >
+        <StyledImage imageURL={props.imageURL} />
+      </div>
+      <Box
+        sx={{
+          width: "100%",
+          justifyContent: "space-between",
+          display: "flex",
+          flexDirection: "column",
+          gap: 1,
+          paddingX: 2,
+          paddingTop: 1,
+          paddingBottom: 3,
+        }}
+      >
+        <Box
+          sx={{
+            justifyContent: "space-between",
+            display: "flex",
+            flexDirection: "row",
+            gap: 1,
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            color="white"
+            fontWeight={"bold"}
+            fontSize={18}
+            textAlign={"right"}
+          >
+            {props.title}
+          </Typography>
+          {!props.isSubscribed && (
+            <Typography
+              variant="caption"
+              color="white"
+              fontWeight={"bold"}
+              sx={{
+                backgroundColor: "white",
+                borderRadius: 5,
+                paddingY: 1,
+                paddingX: 2,
+                width: 120,
+                height: 40,
+                borderColor: "white",
+                borderWidth: 1,
+                color: "primary.main",
+              }}
+            >
+              سجل الآن
+            </Typography>
+          )}
+        </Box>
+        <Typography
+          color="white"
+          fontWeight={"bold"}
+          fontSize={12}
+          textAlign={"right"}
+          textOverflow={"ellipsis"}
+          noWrap
+        >
+          {props.subTitle}
+        </Typography>
+      </Box>
+    </StyledBox>
+  );
+};
--- src/components/teacher-card.tsx
+++ src/components/teacher-card.tsx
@@ -1,132 +1,167 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import Image from "next/image";
 import { FC, useState } from "react";
 
 interface Props {
-    name: string,
-    imageUrl: any,
-    education?: string,
-    bio: string,
-    height?: Number
+  name: string;
+  imageUrl: any;
+  education?: string;
+  bio: string;
+  height?: Number;
 }
 
 export const TeacherCard: FC<Props> = (props) => {
+  const isMobile = window.innerWidth <= 768;
+  const textThreashold = 50;
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleIsExpanded = () => setIsExpanded(!isExpanded);
 
-    const isMobile = (window.innerWidth <= 768)
-    const textThreashold = 50;
-    const [isExpanded, setIsExpanded] = useState(false)
-    const toggleIsExpanded = () => setIsExpanded(!isExpanded)
+  const threasholdedBio = props.bio.slice(0, textThreashold);
+  const isThreasholded = props.bio.length > threasholdedBio.length;
 
-    const threasholdedBio = props.bio.slice(0, textThreashold)
-    const isThreasholded = props.bio.length > threasholdedBio.length
+  const styles = {
+    div: {
+      width: "200px",
+      height: "200px",
+      borderRadius: "50%",
+      overflow: "hidden",
+      display: "flex",
+      "align-items": "center",
+      "justify-content": "center",
+    },
+    image: {
+      width: "100%",
+      height: "auto",
+      "object-fit": "fill",
+    },
+    main: {
+      width: 300,
+      padding: 2,
+      borderRadius: 5,
+      display: "flex",
+      flexDirection: "column",
+      "align-items": "center",
+      "justify-content": "center",
+      backgroundColor: "primary.main",
+    },
+    innerGrid: {
+      display: "flex",
+      flexDirection: "column",
+      "align-items": "center",
+      "justify-content": "center",
+      marginTop: 3,
+    },
+    text: {
+      color: "white",
+      paddingTop: isMobile ? 1 : 2,
+      textAlign: "center",
+    },
 
-    const styles = {
-        div: {
-            width: '200px',
-            height: '200px',
-            borderRadius: '50%',
-            overflow: 'hidden',
-            display: 'flex',
-            'align-items': 'center',
-            'justify-content': 'center',
-        },
-        image: {
-            width: '100%',
-            height: 'auto',
-            'object-fit': 'fill',
-        },
-        main: {
-            width: isMobile ? 300 : 500,
-            padding: isMobile ? 2 : 5,
-            borderRadius: 5,
-            display: 'flex',
-            flexDirection: 'column',
-            'align-items': 'center',
-            'justify-content': 'center',
-            backgroundColor: 'primary.main'
-        },
-        innerGrid: {
-            display: 'flex',
-            flexDirection: 'column',
-            'align-items': 'center',
-            'justify-content': 'center',
-            marginTop: 3
-        },
-        text: {
-            color: 'white',
-            paddingTop: isMobile ? 1 : 2,
-            textAlign: 'center'
-        }
-    }
+    imageDiv: {
+      width: "250px",
+      height: "250px",
+      borderRadius: "50%",
+      overflow: "hidden",
+      display: "flex",
+      "align-items": "center",
+      "justify-content": "center",
+    },
 
-    return (
-        <Grid container sx={styles.main} onClick={toggleIsExpanded}>
-            <div style={styles.div}>
-                <Image src={props.imageUrl} alt={props.name} width={200} height={200} style={styles.image} ></Image>
+    horizontalCard: {
+      width: "100%",
+      margin: "3%",
+      marginY: "1%",
+      padding: "3%",
+      backgroundColor: "primary.main",
+      borderRadius: 5,
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "nowrap",
+      gap: 10,
+    },
+
+    verticalLine: {
+      with: "1%",
+      borderRight: "1px solid white",
+    },
+
+    education: {
+      marginTop: "10%",
+      paddingX: "8%",
+      paddingY: "4%",
+      border: "1px solid white",
+      borderRadius: 3,
+      color: "white",
+    },
+  };
+
+  return isMobile ? (
+    <Grid container sx={styles.main} onClick={toggleIsExpanded}>
+      <div style={styles.div}>
+        <Image
+          src={props.imageUrl}
+          alt={props.name}
+          width={200}
+          height={200}
+          style={styles.image}
+        ></Image>
+      </div>
+      <Grid item container sx={styles.innerGrid}>
+        <Typography sx={styles.text} fontSize={30} fontWeight={"bold"}>
+          {props.name}
+        </Typography>
+        <Typography sx={styles.text} fontSize={20} fontWeight={"regular"}>
+          {props.education}
+        </Typography>
+        {isMobile && (
+          <Typography sx={styles.text} fontSize={18} fontWeight={"regular"}>
+            {isExpanded ? props.bio : threasholdedBio}
+            <div style={{ fontWeight: "bolder", display: "inline" }}>
+              {isThreasholded && !isExpanded && " ... عرض المزيد"}
             </div>
-            <Grid item container sx={styles.innerGrid}>
-                <Typography sx={styles.text} fontSize={30} fontWeight={'bold'}>{props.name}</Typography>
-                <Typography sx={styles.text} fontSize={20} fontWeight={'regular'}>{props.education}</Typography>
-                {
-                    isMobile &&
-                    <Typography sx={styles.text} fontSize={18} fontWeight={'regular'}>
-                        {isExpanded ? props.bio : threasholdedBio}
-                        <div style={{ fontWeight: 'bolder', display: 'inline' }}>{isThreasholded && !isExpanded && ' ... عرض المزيد'}</div>
-                    </Typography>
-                }
-                {
-                    !isMobile && <Typography sx={styles.text} fontSize={18} fontWeight={'regular'}>
-                        {props.bio}
-                    </Typography>
-                }
-            </Grid>
-        </Grid>
-    )
-
-    if (!isMobile)
-        return (
-            <Card sx={{ display: 'flex', width: "60%", backgroundColor: 'primary.main' }}>
-                <CardMedia
-                    component="img"
-                    sx={{ height: 300, width: 300, objectFit: 'contain' }}
-                    image={props.imageUrl}
-                    alt={props.name}
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column', padding: 5 }}>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography gutterBottom variant="h4" component="div" color="primary" textAlign={'right'} fontWeight={'bold'}>
-                            {props.name}
-                        </Typography>
-                        <Typography gutterBottom variant="h6" component="div" color="text.secondary" textAlign={'right'}>
-                            {props.education}
-                        </Typography>
-                        <Typography variant="body1" color="text.secondary">
-                            {props.bio}
-                        </Typography>
-                    </CardContent>
-                </Box>
-            </Card>
-        )
-    else
-        return (
-            <Card sx={{ width: '80%' }} onClick={toggleIsExpanded}>
-                <CardMedia
-                    sx={{ height: props.height || 400 }}
-                    image={props.imageUrl}
-                    title={props.name}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h4" component="div" color="primary" textAlign={'center'}>
-                        {props.name}
-                    </Typography>
-                    <Typography gutterBottom variant="h6" component="div" color="text.secondary" textAlign={'center'}>
-                        {props.education}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {isExpanded ? props.bio : threasholdedBio}
-                        {isThreasholded && !isExpanded && ' ... عرض المزيد'}
-                    </Typography>
-                </CardContent>
-            </Card>
-        )
-}
+          </Typography>
+        )}
+        {!isMobile && (
+          <Typography sx={styles.text} fontSize={18} fontWeight={"regular"}>
+            {props.bio}
+          </Typography>
+        )}
+      </Grid>
+    </Grid>
+  ) : (
+    <Grid container sx={styles.horizontalCard}>
+      <Grid sx={styles.innerGrid}>
+        <div style={styles.imageDiv}>
+          <Image
+            src={props.imageUrl}
+            alt={props.name}
+            width={200}
+            height={200}
+            style={styles.image}
+          ></Image>
+        </div>
+        <Typography sx={styles.text} fontSize={30} fontWeight={"bold"}>
+          {props.name}
+        </Typography>
+        <Typography sx={styles.education} fontSize={20} fontWeight={"regular"}>
+          {props.education}
+        </Typography>
+      </Grid>
+      <Grid sx={styles.verticalLine}></Grid>
+      <Grid sx={styles.innerGrid}>
+        <Typography sx={styles.text} fontSize={18} fontWeight={"regular"}>
+          {props.bio}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
