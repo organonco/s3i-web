d662abf221f1c9031daf078a99789e434f868528
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-05-12 22:26:52 +0300

    add who are we pdf

--- src/app/[lng]/about-us/page.tsx
+++ src/app/[lng]/about-us/page.tsx
@@ -2,43 +2,43 @@
 import { Divider, Grid, Typography } from "@mui/material";
 import Link from "next/link";
 import { useTranslation } from "@/app/i18n/client";
-
+import Image from "next/image";
 
 export default function Page({ params: { lng } }) {
+  const isMobile = window.innerWidth <= 768;
 
-    const isMobile = (window.innerWidth <= 768)
+  const { t } = useTranslation(lng, "translation");
 
-    const { t } = useTranslation(lng, 'translation')
+  const sections = [
+    "intro",
+    "who_are_we",
+    "vision",
+    "message",
+    "values",
+    "outro",
+  ];
 
-    const sections = ["intro", "who_are_we", "vision", "message", "values", "outro"]
+  const whoAreWe = require("/src/assets/who_are_we.jpg");
 
-    const styles =  {
-        "labels": {
-          "textAlign": "right",
-          "paddingTop": 4,
-          "marginBottom": 2,
-          "fontSize": 25,
-            "color": "#2D6276",
-          "textDecorationLine": "underline"
-        },
-        "text": {
-          "textAlign": "right",
-          "paddingBottom": 4,
-          "color": "#666666"
-        }
-    }
+  const styles = {
+    labels: {
+      textAlign: "right",
+      paddingTop: 4,
+      marginBottom: 2,
+      fontSize: 25,
+      color: "#2D6276",
+      textDecorationLine: "underline",
+    },
+    text: {
+      textAlign: "right",
+      paddingBottom: 4,
+      color: "#666666",
+    },
+  };
 
-    return (
-        <Grid container px={isMobile ? 4 : 20}>
-            {
-                sections.map((section) =>
-                    <Grid item key={section}>
-                        <Typography sx={styles.labels}>{t('who-are-we.' + section + ".label")}</Typography>
-                        <Typography sx={styles.text}>{t('who-are-we.' + section + ".text")}</Typography>
-                        <Divider></Divider>
-                    </Grid>
-                )
-            }
-        </Grid>
-    )
+  return (
+    <Grid container width={"100%"}>
+      <Image src={whoAreWe} alt="who_are_we" style={{ width: "100%" }} />
+    </Grid>
+  );
 }
