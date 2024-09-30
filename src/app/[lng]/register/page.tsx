c06e19951af0e374db6ff997e0033027761ef594
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-10-01 02:13:56 +0300

    fix policy

--- src/app/[lng]/register/page.tsx
+++ src/app/[lng]/register/page.tsx
@@ -6,13 +6,13 @@ import { RegisterInfo } from "@/logic/interfaces";
 import { useCoursesStore } from "@/logic/store";
 import { sitemap } from "@/site-map";
 import {
-  Button,
-  Card,
-  CardContent,
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  Grid,
+	Button,
+	Card,
+	CardContent,
+	Checkbox,
+	FormControlLabel,
+	FormGroup,
+	Grid,
 } from "@mui/material";
 import dayjs from "dayjs";
 import { Form, Formik, FormikValues } from "formik";
@@ -24,161 +24,158 @@ import { ReferenceSelect } from "@/components/reference-select";
 import Link from "next/link";
 
 export default function Page({ params: { lng } }: { params: { lng: any } }) {
-  // const fetchCategoriesInfo = useCoursesStore(state => state.fetchCategoriesInfo)
-  const { t } = useTranslation(lng, "translation");
-  const { push } = useRouter();
-  const initialValues: RegisterInfo = {
-    name_ar: "",
-    name_en: "",
-    phone: "",
-    email: "",
-    date_of_birth: dayjs(Date.now()),
-    nationality_id: "",
-    education_level_id: "",
-    reference: "",
-    address: "",
-    password: "",
-    password_confirmation: "",
-  };
-  const { registerUser, fetchRegisterInfo, registerInfo } = useCoursesStore();
+	// const fetchCategoriesInfo = useCoursesStore(state => state.fetchCategoriesInfo)
+	const { t } = useTranslation(lng, "translation");
+	const { push } = useRouter();
+	const initialValues: RegisterInfo = {
+		name_ar: "",
+		name_en: "",
+		phone: "",
+		email: "",
+		date_of_birth: dayjs(Date.now()),
+		nationality_id: "",
+		education_level_id: "",
+		reference: "",
+		address: "",
+		password: "",
+		password_confirmation: "",
+	};
+	const { registerUser, fetchRegisterInfo, registerInfo } = useCoursesStore();
 
-  const handleSubmit = (values: FormikValues, { setSubmitting }) => {
-    const redirectToVerify = (id: string) => push(sitemap.verify.url + id)
-    const newValues = {
-      ...values,
-      date_of_birth: moment(values.date_of_birth).format("YYYY-MM-DD"),
-    };
-    registerUser(newValues, redirectToVerify, (value: boolean) =>
-      setSubmitting(value)
-    );
-  };
-  useEffect(() => {
-    fetchRegisterInfo();
-  }, []);
+	const handleSubmit = (values: FormikValues, { setSubmitting }) => {
+		const redirectToVerify = (id: string) => push(sitemap.verify.url + id)
+		const newValues = {
+			...values,
+			date_of_birth: moment(values.date_of_birth).format("YYYY-MM-DD"),
+		};
+		registerUser(newValues, redirectToVerify, (value: boolean) =>
+			setSubmitting(value)
+		);
+	};
+	useEffect(() => {
+		fetchRegisterInfo();
+	}, []);
 
-  const isMobile = window.innerWidth <= 768;
+	const isMobile = window.innerWidth <= 768;
 
-  // const file1 = require("/src/assets/rules.pdf");
-  // const file2 = require("/src/assets/privacy-policy.pdf");
-
-  return (
-    <Grid
-      container
-      justifyContent={"center"}
-      alignContent="center"
-      height="100%"
-      width={"100%"}
-    >
-      <Card
-        sx={{
-          minWidth: isMobile ? "100%" : 600,
-          width: isMobile ? "100%" : "20%",
-        }}
-      >
-        <CardContent>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={registerValidationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <Grid item direction={"column"} sx={{ width: "100%" }}>
-                  <Grid
-                    container
-                    direction={"column"}
-                    gap={2}
-                    lg={12}
-                    sx={{ width: "100%" }}
-                  >
-                    <StyledTextField
-                      translateKey="name_ar"
-                      name="name_ar"
-                      required
-                    />
-                    <StyledTextField
-                      translateKey="name_en"
-                      name="name_en"
-                      required
-                    />
-                    <StyledTextField
-                      translateKey="phone"
-                      name="phone"
-                      required
-                    />
-                    <StyledTextField translateKey="email" name="email" />
-                    <StyledDatePicker
-                      translateKey="date_of_birth"
-                      name="date_of_birth"
-                    />
-                    <StyledSelect
-                      translateKey="nationality"
-                      name="nationality_id"
-                      options={registerInfo?.nationalities ?? []}
-                    />
-                    <StyledTextField translateKey="address" name="address" />
-                    <StyledSelect
-                      translateKey="education_level"
-                      name="education_level_id"
-                      options={registerInfo?.education_levels ?? []}
-                    />
-                    <ReferenceSelect
-                      name="reference"
-                      translateKey="reference"
-                    />
-                    <StyledTextField
-                      translateKey="password"
-                      name="password"
-                      required
-                      type="password"
-                    />
-                    <StyledTextField
-                      translateKey="password_confirmation"
-                      name="password_confirmation"
-                      required
-                      type="password"
-                    />
-                    <FormGroup>
-                      <FormControlLabel
-                        control={<Checkbox name="lgpd_agreement" />}
-                        label={
-                          <>
-                            أنا أوافق على{" "}
-                            <Link
-                              href={sitemap.terms.url}
-                              target="_blank"
-                              style={{ color: "blue" }}
-                            >
-                              سياسة وشروط استخدام
-                            </Link>{" "}
-                            و
-                            <Link
-                              href={sitemap.privacy.url}
-                              target="_blank"
-                              style={{ color: "blue" }}
-                            >
-                              سياسة خصوصية
-                            </Link>{" "}
-                            منصة سعي للتدريب والتأهيل
-                          </>
-                        }
-                      />
-                    </FormGroup>
-                    <Button
-                      variant="contained"
-                      type="submit"
-                      disabled={isSubmitting}
-                      sx={{ mt: 4 }}
-                    >
-                      {t("buttons.register")}
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Form>
-            )}
-          </Formik>
-        </CardContent>
-      </Card>
-    </Grid>
-  );
+	return (
+		<Grid
+			container
+			justifyContent={"center"}
+			alignContent="center"
+			height="100%"
+			width={"100%"}
+		>
+			<Card
+				sx={{
+					minWidth: isMobile ? "100%" : 600,
+					width: isMobile ? "100%" : "20%",
+				}}
+			>
+				<CardContent>
+					<Formik
+						initialValues={initialValues}
+						validationSchema={registerValidationSchema}
+						onSubmit={handleSubmit}
+					>
+						{({ isSubmitting }) => (
+							<Form>
+								<Grid item direction={"column"} sx={{ width: "100%" }}>
+									<Grid
+										container
+										direction={"column"}
+										gap={2}
+										lg={12}
+										sx={{ width: "100%" }}
+									>
+										<StyledTextField
+											translateKey="name_ar"
+											name="name_ar"
+											required
+										/>
+										<StyledTextField
+											translateKey="name_en"
+											name="name_en"
+											required
+										/>
+										<StyledTextField
+											translateKey="phone"
+											name="phone"
+											required
+										/>
+										<StyledTextField translateKey="email" name="email" />
+										<StyledDatePicker
+											translateKey="date_of_birth"
+											name="date_of_birth"
+										/>
+										<StyledSelect
+											translateKey="nationality"
+											name="nationality_id"
+											options={registerInfo?.nationalities ?? []}
+										/>
+										<StyledTextField translateKey="address" name="address" />
+										<StyledSelect
+											translateKey="education_level"
+											name="education_level_id"
+											options={registerInfo?.education_levels ?? []}
+										/>
+										<ReferenceSelect
+											name="reference"
+											translateKey="reference"
+										/>
+										<StyledTextField
+											translateKey="password"
+											name="password"
+											required
+											type="password"
+										/>
+										<StyledTextField
+											translateKey="password_confirmation"
+											name="password_confirmation"
+											required
+											type="password"
+										/>
+										<FormGroup>
+											<FormControlLabel
+												control={<Checkbox name="lgpd_agreement" />}
+												label={
+													<>
+														أنا أوافق على{" "}
+														<Link
+															href={sitemap.terms.url}
+															target="_blank"
+															style={{ color: "blue" }}
+														>
+															سياسة وشروط استخدام
+														</Link>{" "}
+														و
+														<Link
+															href={sitemap.privacy.url}
+															target="_blank"
+															style={{ color: "blue" }}
+														>
+															سياسة خصوصية
+														</Link>{" "}
+														منصة سعي للتدريب والتأهيل
+													</>
+												}
+											/>
+										</FormGroup>
+										<Button
+											variant="contained"
+											type="submit"
+											disabled={isSubmitting}
+											sx={{ mt: 4 }}
+										>
+											{t("buttons.register")}
+										</Button>
+									</Grid>
+								</Grid>
+							</Form>
+						)}
+					</Formik>
+				</CardContent>
+			</Card>
+		</Grid>
+	);
 }
